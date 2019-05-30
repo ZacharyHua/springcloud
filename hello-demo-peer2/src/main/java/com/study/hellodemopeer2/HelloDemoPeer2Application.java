0854f8c80547298b9a1e8adaae972a4b0c88b133
Sun_pig <806746185@qq.com>
2019-05-30 22:24:51 +0800

    add new project hello-demo-peer2

---
+++ hello-demo-peer2/src/main/java/com/study/hellodemopeer2/HelloDemoPeer2Application.java
@@ -0,0 +1,13 @@
+package com.study.hellodemopeer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDemoPeer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDemoPeer2Application.class, args);
+    }
+
+}
