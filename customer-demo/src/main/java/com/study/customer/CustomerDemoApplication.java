dc97c0d061043481699c9be6db5ba224657d2d62
Sun_pig <806746185@qq.com>
2019-06-03 10:18:14 +0800

    ribbon - 01

--- customer-demo/src/main/java/com/study/customer/CustomerDemoApplication.java
+++ customer-demo/src/main/java/com/study/customer/CustomerDemoApplication.java
@@ -1,14 +1,21 @@
 package com.study.customer;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@RibbonClients(
+        @RibbonClient(value = "hello-server")
+)
 public class CustomerDemoApplication {
 
     public static void main(String[] args) {
@@ -19,5 +26,17 @@ public class CustomerDemoApplication {
     @LoadBalanced
     public RestTemplate template(){
         return new RestTemplate();
+    }
+
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate template(){
+//        return new RestTemplate();
+//    }
+
+    @Bean
+    public IRule ribbonRule(){
+        //自定义规则
+        return new RandomRule();
     }
 }
--- customer-demo/src/main/java/com/study/customer/controller/CustomerController.java
+++ customer-demo/src/main/java/com/study/customer/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.study.customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +11,21 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerController {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
     @GetMapping("index")
     public Object getIndex(){
-        return restTemplate.getForObject("http://HELLOSERVER/",String.class,"");
+        ServiceInstance serviceInstance = loadBalancerClient.choose("hello-server");
+        String ip = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        return restTemplate.getForObject("http://"+ip+":"+port,String.class,"");
+
     }
+
+//    @GetMapping("index")
+//    public Object getIndex(){
+//        return restTemplate.getForObject("http://HELLOSERVER/",String.class,"");
+//    }
 }
