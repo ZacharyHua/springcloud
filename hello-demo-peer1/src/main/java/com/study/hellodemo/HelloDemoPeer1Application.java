378c3aa8e636414a1ece041c58439ea84826d62d
Sun_pig <806746185@qq.com>
2019-06-25 15:05:42 +0800

    config-server demo

--- hello-demo-peer1/src/main/java/com/study/hellodemo/HelloDemoPeer1Application.java
+++ hello-demo-peer1/src/main/java/com/study/hellodemo/HelloDemoPeer1Application.java
@@ -5,6 +5,7 @@ import com.study.hellodemo.accept.StreamClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.ApplicationEvent;
@@ -23,6 +24,7 @@ import java.util.Random;
 @RestController
 @EnableBinding(value = {StreamClient.class})
 @RemoteApplicationEventScan("com.study")
+@EnableDiscoveryClient
 public class HelloDemoPeer1Application {
 
     public static void main(String[] args) {
